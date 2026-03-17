99cf036fef74be897ed9f0b70845a2ddc4bc6690
Cyril Fischer <github@cyril-fischer.fr>
2026-03-17 23:27:14 +0100

    feat: add migrations for tcgs, sets, products, collection_items

---
+++ database/migrations/2026_03_17_205631_create_collection_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collection_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity')->default(1);
+            $table->text('notes')->nullable();
+            $table->decimal('price_paid', 8, 2);
+            $table->date('purchase_date')->nullable();
+            $table->string('product_condition');
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collection_items');
+    }
+};
