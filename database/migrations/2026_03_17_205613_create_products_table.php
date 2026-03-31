96566a212f3693e72dff43d9df29d587c00a1be1
Cyril Fischer <github@cyril-fischer.fr>
2026-03-31 22:58:33 +0200

    feat: make Product.name nullable (example for ETB)

--- database/migrations/2026_03_17_205613_create_products_table.php
+++ database/migrations/2026_03_17_205613_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->decimal('base_price', 8, 2)->nullable();
             $table->date('release_date')->nullable();
             $table->string('product_type');
